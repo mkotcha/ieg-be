c728a60b3a0c7bca1fc515b8bb987cb227e77083
Matteo Falasca <matteo.falasca@emmek.org>
2023-12-02 23:18:50 +0100

    rimossa entita indirizzo

--- src/main/java/org/emmek/IEG/repositories/IndirizzoRepository.java
+++
@@ -1,9 +0,0 @@
-package org.emmek.IEG.repositories;
-
-import org.emmek.IEG.entities.Indirizzo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface IndirizzoRepository extends JpaRepository<Indirizzo, UUID> {
-}
